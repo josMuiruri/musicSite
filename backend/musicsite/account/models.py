4e4cf8e3c244c88e5db44847650a19b80143ab27
josMuiruri <josemuiruri51@gmail.com>
2025-04-30 13:57:48 +0300

    adding an account app

--- backend/musicsite/account/models.py
+++ backend/musicsite/account/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    profile_picture = models.ImageField(upload_to='user_profiles/', null=True, blank=True)
+    is_musician = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
